a3e3f03310599d458740fe49389a17bf0ba0de35
KiranTotager <tskiran5114@gmail.com>
2025-08-05 14:44:02 +0530

    july 08

--- src/Practice/arrays/SubArrays.java
+++ src/Practice/arrays/SubArrays.java
@@ -5,9 +5,12 @@ import java.util.Random;
 
 public class SubArrays {
     public static void main(String[] args){
-        int[] arr=new Random().ints(5,-20,5).toArray();
+        int[] arr=new Random().ints(5,-20,20).toArray();
 //        int[] arr={}
+        int s=18;
+        int count=0;
         int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
         System.out.println(Arrays.toString(arr));
         for(int i=0;i<arr.length;i++){
             for(int j=i;j<arr.length;j++){
@@ -16,11 +19,15 @@ public class SubArrays {
                     sum+=arr[k];
 //                    System.out.print(arr[k]+" ");
                 }
+                if(sum==s) count++;
+                min=Math.min(sum,min);
                 max=Math.max(sum,max);
 //                System.out.print("sum is "+sum);
 //                System.out.println();
             }
         }
         System.out.println("the maximum sum of sub array is"+max);
+        System.out.println("the minimum sum of sub array is"+min);
+        System.out.println("the count of sum which is equal "+count);
     }
 }
